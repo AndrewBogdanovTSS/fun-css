0885c67cf84e469d2ba8d86fae38e1a3545b6db5
Andrew Bogdanov <andrewbogdanov@ukr.net>
2025-05-19 22:10:22 +0200

    switching to presetWind4

--- uno.config.ts
+++ uno.config.ts
@@ -1,9 +1,9 @@
-import { defineConfig, presetWind3 } from 'unocss'
+import { defineConfig, presetWind4 } from 'unocss'
 import { funPreset } from './uno/presets/fun'
 
 export default defineConfig({
   presets: [
-    presetWind3(),
+    presetWind4(),
     funPreset(),
   ],
 })
