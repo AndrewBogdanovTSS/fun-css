693f0d0bc6e949134538cde0603e32150e0810e4
Andrew Bogdanov <andrewbogdanov@ukr.net>
2025-05-19 22:09:54 +0200

    adding example of eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,4 +1,17 @@
 import unocss from '@unocss/eslint-config/flat'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(unocss)
+export default withNuxt(unocss, {
+  files: ['**/*.vue'],
+  rules: {
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 5,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+  },
+})
