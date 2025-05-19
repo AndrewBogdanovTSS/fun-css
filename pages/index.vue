2a0e8dc3669f2049bdcff4d3a90910f268b7ee3c
Andrew Bogdanov <andrewbogdanov@ukr.net>
2025-05-19 22:11:22 +0200

    cleanup

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,6 @@
 <template>
-  <div
-    class="h-screen f-col center duration"
-    :class="active ? 'bg-brand-1' : 'bg-brand-2'"
-  >
-    <div>
+  <div class="duration active f-col h-screen center" :class="active ? 'bg-brand-1' : 'bg-brand-2'">
+    <div class="opt-1 opt-2 group-@active:text-sm">
       <functional-switch v-model="active" />
       <template v-if="active">
         <h2>Functional buttons</h2>
@@ -20,9 +17,11 @@
         </div>
       </template>
       <template v-else>
-        <h2>Traditional buttons</h2>
+        <h2 class="@sm:c-red-1">
+          Traditional buttons
+        </h2>
         <div class="flex gap-4">
-          <traditional-btn variant="one">
+          <traditional-btn variant="one" class="|@hactive:c-red-3">
             Variant One
           </traditional-btn>
           <traditional-btn variant="two">
