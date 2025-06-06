006e59bc037c3dbef83e0f691fe4d4d8f40278ca
Andrew Bogdanov <andrewbogdanov@ukr.net>
2025-06-06 23:07:36 +0200

    adding bg component

---
+++ components/cms/Bg.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    class="full"
+    :class="{ 'bg-$bg': color?.sm, 'md:bg-$bg-md': color?.md, 'lg:bg-$bg-lg': color?.lg }"
+    :style="{ '--bg': color?.sm, '--bg-md': color?.md, '--bg-lg': color?.lg }"
+  />
+</template>
+
+<script lang="ts" setup>
+import type { Responsive } from '~/types'
+
+defineProps<{
+  color: Responsive
+}>()
+</script>
