0a197907ecce0a1839f876460a57c71993f03c0f
Andrew Bogdanov <andrewbogdanov@ukr.net>
2025-06-11 22:40:04 +0200

    adding eslint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import withNuxt from './.nuxt/eslint.config.mjs'
 export default withNuxt(unocss, {
   files: ['**/*.vue'],
   rules: {
+    '@typescript-eslint/no-explicit-any': 'off',
     'vue/component-name-in-template-casing': ['error', 'kebab-case'],
     'vue/max-attributes-per-line': ['error', {
       singleline: {
